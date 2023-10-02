f5e287df09940955387122e79bb1061c80e47cf4
supershegs <segsamos@gmail.com>
2023-10-02 11:14:20 +0100

    added another condition to run direct

--- videoApp/views.py
+++ videoApp/views.py
@@ -62,6 +62,39 @@ class VideoChunkUploadView(APIView):
             if all_chunks_received(file_id):
                 assemble_file(file_id)
             return Response("Chunk received and processed", status=status.HTTP_200_OK)
+        elif request.data != '':
+            serializer = VideoChunkSerializer(data=request.data)
+            if serializer.is_valid():
+                video_chunk_instance = serializer.save()# Save the video chunk to the 'media/chunks/' directory
+                video_path = video_chunk_instance.video_chunk.path  # Convert video chunk to audio
+                print(video_path)
+                audio_path = os.path.splitext(video_path)[0] + ".wav"  # Change the audio format to WAV
+                # audio_path = os.path.splitext(video_path)[0] + ".mp3"
+                print(audio_path)
+                video_clip = mp.VideoFileClip(video_path)
+                audio_clip = video_clip.audio
+                audio_clip.write_audiofile(audio_path)
+
+                # Transcribe audio to manuscript
+                recognizer = sr.Recognizer()
+                print(recognizer)
+                with sr.AudioFile(audio_path) as source:
+                    audio = recognizer.record(source)
+                    # manuscript = recognizer.recognize_google(audio)
+                    try:
+                        manuscript = recognizer.recognize_google(audio)
+                        print(manuscript)
+                    except sr.RequestError as e:
+                        return Response("Could not request results from Google Speech Recognition service; {0}".format(e), status=status.HTTP_400_BAD_REQUEST)
+                    except sr.UnknownValueError:
+                        print("Google Speech Recognition could not understand audio")
+
+
+            # Update the video chunk instance with the manuscript
+            video_chunk_instance.manuscript = manuscript
+            video_chunk_instance.save()
+
+            return Response(VideoChunkSerializer(video_chunk_instance).data, status=status.HTTP_201_CREATED)
         else:
             # Handle unsupported content type
             print('Printed file sent if not supported content type', request.data)
