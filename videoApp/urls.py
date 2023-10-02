7a8ff51480bf83604d02fa6014b0170c50112da8
supershegs <segsamos@gmail.com>
2023-10-02 14:28:12 +0100

    code modify

--- videoApp/urls.py
+++ videoApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path('upload/', VideoChunkUploadView.as_view(), name='video-chunk-upload'),
+    path('docs/', VideoChunkUploadView.as_view(), name='video-chunk-upload'),
 ]
